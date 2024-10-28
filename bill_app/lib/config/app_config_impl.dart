fa04ee8dcd57f8a55bda6ad95dfe28f7c2e709fd
CamiloArias12 <ariaspira13@gmail.com>
2024-10-28 14:33:21 -0500

    feat:add app services bill

--- bill_app/lib/config/app_config_impl.dart
+++ bill_app/lib/config/app_config_impl.dart
@@ -1,15 +1,13 @@
 import 'package:bill_app/config/app_config.dart';
 import 'package:bill_app/config/style_app/style_app.dart';
-import 'package:flutter/material.dart';
 import 'package:get_it/get_it.dart';
 
 class AppConfigImpl extends AppConfig {
   late final StyleApp styleApp;
 
-  AppConfigImpl({required super.isBackground}) {
-    appName = "Gruber";
+  AppConfigImpl() {
+    appName = "Bill";
     apiUrl = const String.fromEnvironment('API_URL');
     styleApp = GetIt.I.get<StyleApp>();
-    spinnerLoading = CircularProgressIndicator(color: styleApp.primaryColor);
   }
 }
--- bill_app/lib/config/style_app/style_app_impl.dart
+++ bill_app/lib/config/style_app/style_app_impl.dart
@@ -4,12 +4,5 @@ import 'package:flutter/material.dart';
 class StyleAppImpl extends StyleApp {
   StyleAppImpl() {
     primaryColor = const Color(0xFF1A9BD8);
-    secundaryColor = const Color(0xFF464444);
-    grayColor = const Color(0xFFECECEC);
-    blackColor = const Color(0xFF211F1F);
-    whiteColor = const Color(0xFFF8F8F8);
-    redColor = const Color(0xFFCA0C0C);
-    grayLightColor = const Color(0xFF777777);
-    unseletedColor = const Color(0xFFCFCFCF);
   }
 }
