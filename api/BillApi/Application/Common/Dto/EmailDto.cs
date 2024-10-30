cfe75185ae1d5d88a84fd22a1d6005627304f949
CamiloArias12 <ariaspira13@gmail.com>
2024-10-29 23:13:17 -0500

    feat:delete initialization

--- api/BillApi/Application/Common/Dto/EmailDto.cs
+++ api/BillApi/Application/Common/Dto/EmailDto.cs
@@ -2,8 +2,8 @@ namespace BillApi.Infrastructure.Email
 {
     public class EmailDTO
     {
-        public required string To { get; set; } = string.Empty;
-        public required string Subject { get; set; } = string.Empty;
-        public required string Body { get; set; } = string.Empty;
+        public required string To { get; set; }
+        public required string Subject { get; set; }
+        public required string Body { get; set; }
     }
 }
